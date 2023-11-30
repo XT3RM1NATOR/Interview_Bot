0df360c11b736d2cc55527b853b100ce00adc52c
Michael Shamshidov <mshamshidov22@ku.edu.tr>
2023-11-30 11:14:14 +0500

    update

--- src/handlers/adminHandler.ts
+++ src/handlers/adminHandler.ts
@@ -1,23 +1,4 @@
-import { addUserToDatabase } from "../service/service";
-
-export const adminHandler = (ctx: any) => {
+export const adminHandler = async (ctx: any) => {
+  ctx.session.adminStage = true
   ctx.reply('You chose Admin. Please enter the password:');
-  
-  // Listen for the user's response after choosing admin
-  ctx.once('text', async (ctx: any) => {
-    const enteredPassword = ctx.message.text;
-    
-    // Check if the entered password is correct
-    const correctPassword = '123'; // Replace this with your actual password
-    
-    if (enteredPassword === correctPassword) {
-      ctx.reply('Password correct. You are now an admin!');
-      // Add the user to the database as an admin
-      const user = await addUserToDatabase(ctx.from?.username, 'admin');
-      console.log(user)
-      // Perform any other actions after successfully adding the user as an admin
-    } else {
-      ctx.reply('Incorrect password. Access denied.');
-    }
-  });
-};
+}
