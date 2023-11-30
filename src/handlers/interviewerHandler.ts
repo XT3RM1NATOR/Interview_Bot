bcee3fa2deb321a2cd43c4e5408f51a6dfd677b6
Michael Shamshidov <mshamshidov22@ku.edu.tr>
2023-11-30 14:54:45 +0500

    update

--- src/handlers/interviewerHandler.ts
+++ src/handlers/interviewerHandler.ts
@@ -2,6 +2,4 @@ export const interviewerHandler = (ctx: any) => {
   ctx.session.interviewer = true;
   ctx.session.gmtStage = true;
   ctx.reply('write your gmt time zone in the format "5" or "-4.30');
-  // Perform interviewer-related actions
-  // Add your interviewer logic here
 };
