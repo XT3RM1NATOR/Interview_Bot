b3e49666072933ad4ecb51d26bef9349e000ce4b
Michael Shamshidov <mshamshidov22@ku.edu.tr>
2023-12-05 19:41:31 +0500

    update

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,5 +1,6 @@
 import { logger } from '../config/logger-config';
 
-export const logAction = (username:string, action: string) => {
-  logger.info(`${username}: ${action}`);
+export const logAction = (username: string, action: string) => {
+  const currentTime = new Date().toLocaleString(); // Get current time
+  logger.info(`${currentTime} - ${username}: ${action}`); // Log with timestamp
 };
