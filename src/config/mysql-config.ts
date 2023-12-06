22b9c693e4b5753e007082328361c9d9d19dcfe6
Michael Shamshidov <mshamshidov22@ku.edu.tr>
2023-12-06 17:19:48 +0500

    update

--- src/config/mysql-config.ts
+++ src/config/mysql-config.ts
@@ -1,16 +1,19 @@
+import dotenv from 'dotenv';
 import { DataSource } from "typeorm";
 import { ChatList } from "../entity/ChatList";
 import { InterviewerSlot } from "../entity/InterviewerSlot";
 import { Session } from "../entity/Session";
 import { User } from "../entity/User";
 
+dotenv.config();
+
 
 const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "milkshake",
+  host: process.env.MYSQL_HOST,
+  port: parseInt(process.env.MYSQL_PORT!),
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
   database: "node_ru",
   entities: [User, ChatList, InterviewerSlot, Session]
 })
